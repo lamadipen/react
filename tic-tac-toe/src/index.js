41639c3f539d8dc096af8f0106b23296da262fd3
lamadipen <lamadipen@yahoo.com>
2018-03-19 18:32:34 -0500

    lifting up state

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => this.props.onClick()}> 
         {this.props.value}
       </button>
     );
@@ -14,8 +21,22 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]}
+                   onClick={() => this.handleClick(i)} 
+    />;
   }
 
   render() {
