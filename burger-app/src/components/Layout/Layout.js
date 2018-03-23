77108f83d6bb4cd137c8fcf3330d8e4332bbbd67
lamadipen <lamadipen@yahoo.com>
2018-03-23 11:42:26 -0500

    closed #7 created base layout component

---
+++ burger-app/src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+
+const layout = (props) => {
+	return (
+		<Aux>
+			<div>toolbar, sidedrawer ,backdrop</div>
+			<main> {props.children} </main>
+		</Aux>
+	);
+}
+
+export default layout;
