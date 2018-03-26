b896d18664fe2cf0f07a14859d956d2ac0590b2c
lamadipen <lamadipen@yahoo.com>
2018-03-26 15:13:05 -0500

    closed #15 outputting multiple controls

--- burger-app/src/components/Burger/BuildControls/BuildContorls.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const buildControls = (props) =>{
-
-
-}
-
-export default buildContorls;
--- burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import classes from './BuildControl.css';
 
-cosnt buildControl = (props) => (
+const buildControl = (props) => (
 		<div className={classes.BuildControl}>
-			<div className{classes.la>{props.Label}</div>
+			<div className={classes.Label}>{props.Label}</div>
 			<button className={classes.Less}>Less</button>
-			<button className={classes.More>More</button>	
+			<button className={classes.More}>More</button>	
 		</div>
-	);
+);
 
 export default buildControl;
