595a04064cf1e1c7cdde428ecc458f31fdd2515f
lamadipen <lamadipen@yahoo.com>
2018-04-16 14:58:26 -0500

    closed #31 adding hamburger icon

--- burger-app/src/components/Navigation/ToolBar/ToolBar.js
+++ burger-app/src/components/Navigation/ToolBar/ToolBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import DrawerToggle from '../ToolBar/DrawerToggle/DrawerToggle';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
