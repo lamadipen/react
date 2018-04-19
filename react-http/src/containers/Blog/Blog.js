2b7d5b44653f8b319f0a82e65d75a2bf759a8193
lamadipen <lamadipen@yahoo.com>
2018-04-18 21:51:58 -0500

    adding http/ajax

---
+++ react-http/src/containers/Blog/Blog.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(response => {
+            const posts = response.data.slice(0,4);
+            const updatedPosts = posts.map(post => {
+                return {...post,
+                        author:'dipen'
+
+                }
+            });
+            this.setState({posts: updatedPosts});
+        });
+    }
+    render () {
+        const posts = this.state.posts.map(post => {
+            return <Post title={post.title} 
+                        author={post.author} 
+                        key={post.id}
+                        clicked ={()=>this.postSelectedHandler(post.id)}
+                        />;
+        });
+        return (
+            <div>
+                <section className="Posts">
+                   {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+}
+
+export default Blog;
