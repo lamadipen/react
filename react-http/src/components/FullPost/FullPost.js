e65dc3b0b55b897bc1b9b688ed0d03927a095d93
Dipen Lama <dipenlama@Dipens-MacBook-Pro.local>
2018-04-19 21:40:38 -0500

    adding delete request

--- react-http/src/components/FullPost/FullPost.js
+++ react-http/src/components/FullPost/FullPost.js
@@ -16,6 +16,16 @@ class FullPost extends Component {
             }
         }
     }
+
+
+    deleteDataHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts'+this.props.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if(this.props.id){
@@ -27,7 +37,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deleteDataHandler}>Delete</button>
                     </div>
                 </div>
     
